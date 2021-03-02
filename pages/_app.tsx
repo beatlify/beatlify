4a4e3c345053dbc786bdb68b0ba6dad4ed037d16
manen <varga.bercel.i@gmail.com>
2021-03-02 23:14:44 +0100

    install chakra-ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
 import "../styles/globals.css";
 import React from "react";
 import type { AppProps } from "next/app";
+import { CSSReset, theme, ThemeProvider } from "@chakra-ui/react";
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <CSSReset />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 };
 
 export default MyApp;
