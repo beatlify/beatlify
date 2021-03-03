69b364896f901f91cc24fde42943f50807503bdf
manen <varga.bercel.i@gmail.com>
2021-03-03 02:54:47 +0100

    Terms of Service, User and Lawyer modes

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,7 @@
 import { Box, Heading } from "@chakra-ui/react";
 
 const Header: React.FC = () => (
-  <Box textAlign="center" alignItems="center" bg="#2a2a2a">
+  <Box textAlign="center" alignItems="center" bg="#2a2a2a" marginBottom="5">
     <Heading size="md" p="2">
       Beatlify
     </Heading>
