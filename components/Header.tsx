2063ec0a059d958062506ff2134284d5459a4586
manen <varga.bercel.i@gmail.com>
2021-03-03 03:20:29 +0100

    Footer navigation Header click

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,10 @@
 import { Box, Heading } from "@chakra-ui/react";
+import Link from "next/link";
 
 const Header: React.FC = () => (
   <Box textAlign="center" alignItems="center" bg="#2a2a2a" marginBottom="5">
     <Heading size="md" p="2">
-      Beatlify
+      <Link href="/">Beatlify</Link>
     </Heading>
   </Box>
 );
